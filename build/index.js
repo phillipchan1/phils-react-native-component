d659e42669572d96370c194f54f9fddeeb13aeb8
Phillip Chan <phil.chan@sce.com>
2020-07-31 10:57:29 -0700

    push dist

--- build/index.js
+++ build/index.js
@@ -24,7 +24,7 @@ var MyComponent = /** @class */ (function (_super) {
     }
     MyComponent.prototype.render = function () {
         return (React.createElement(react_native_1.View, null,
-            React.createElement(react_native_1.Text, null, " My Component using typescript!! 2.3 ")));
+            React.createElement(react_native_1.Text, null, " My Component using typescript!! 3.0.2 ")));
     };
     return MyComponent;
 }(React.Component));
--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ export default class MyComponent extends React.Component {
   render() {
     return (
       <View>
-        <Text> My Component using typescript!! 3.0 </Text>
+        <Text> My Component using typescript!! 3.0.2 </Text>
       </View>
     )
   }
